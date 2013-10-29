1c6d5b0d5b2be135e188751676fbdb1090c7784d
cedric <bcedrix@gmail.com>
2013-10-29 22:11:23 +0100

    #5 Some tests about library initialization and voice activity detection analysing have been writted.

--- MediaLearnerLibTests/MediaLearnerLibTests.pro
+++ MediaLearnerLibTests/MediaLearnerLibTests.pro
@@ -17,24 +17,13 @@ SOURCES += tst_medialearnerlibtests.cpp
 DEFINES += SRCDIR=\\\"$$PWD/\\\"
 
 
-release:DESTDIR = ../release
-release:OBJECTS_DIR = ../../tmpcompil/release/obj
-release:MOC_DIR = ../../tmpcompil/release/moc
-release:RCC_DIR = ../../tmpcompil/release/rcc
-release:UI_DIR = ../../tmpcompil/release/ui
+DESTDIR = ../binaries
+OBJECTS_DIR = ../tmpcompil/$$TARGET/obj
+MOC_DIR = ../tmpcompil/$$TARGET/moc
+RCC_DIR = ../tmpcompil/$$TARGET/rcc
+UI_DIR = ../tmpcompil/$$TARGET/ui
 
-debug:DESTDIR = ../debug
-debug:OBJECTS_DIR = ../../tmpcompil/debug/obj
-debug:MOC_DIR = ../../tmpcompil/debug/moc
-debug:RCC_DIR = ../../tmpcompil/debug/rcc
-debug:UI_DIR = ../../tmpcompil/debug/ui
+LIBS += -L$$DESTDIR -lMediaLearnerLib
 
-
-
-win32:CONFIG(release, debug|release): LIBS += -L$$PWD/../Desktop_Qt_5_1_0_GCC_64bit/release/ -lMediaLearnerLib
-else:win32:CONFIG(debug, debug|release): LIBS += -L$$PWD/../Desktop_Qt_5_1_0_GCC_64bit/debug/ -lMediaLearnerLib
-else:unix:CONFIG(release, debug|release): LIBS += -L$$PWD/../Desktop_Qt_5_1_0_GCC_64bit/release/ -lMediaLearnerLib
-else:unix:CONFIG(debug, debug|release): LIBS += -L$$PWD/../Desktop_Qt_5_1_0_GCC_64bit/debug/ -lMediaLearnerLib
-
-INCLUDEPATH += $$PWD/../MediaLearnerLib
-DEPENDPATH += $$PWD/../MediaLearnerLib
+INCLUDEPATH += ../MediaLearnerLib
+DEPENDPATH += ../MediaLearnerLib
--- MediaLearnerLibTests/tst_medialearnerlibtests.cpp
+++ MediaLearnerLibTests/tst_medialearnerlibtests.cpp
@@ -2,6 +2,7 @@
 #include <QtTest>
 #include <QCoreApplication>
 #include <MediaLearnerLib.h>
+#include <QThread>s
 
 class MediaLearnerLibTests : public QObject{
     Q_OBJECT
@@ -11,24 +12,79 @@ public:
     
 private Q_SLOTS:
     void testInitialisation();
-    void testExtraction();
+    void testExtractorExists();
+    void testExtractorPluginsExist();
+    void testSequencesExtractions();
 };
-
 //====================================
 MediaLearnerLibTests::MediaLearnerLibTests(){
 }
 //====================================
 void MediaLearnerLibTests::testInitialisation(){
     MediaLearner::MediaLearnerLib mediaLearner;
-    QVERIFY2(true, "Failure");
+    QVERIFY(true);
 
 }
 //====================================
-void MediaLearnerLibTests::testExtraction(){
-    QVERIFY2(true, "Failure");
+void MediaLearnerLibTests::testExtractorExists(){
+    MediaLearner::MediaLearnerLib mediaLearner;
+    MediaLearner::SequenceExtractor *extractor
+            = mediaLearner.getSequenceExtractor();
+    QVERIFY(extractor != NULL);
 }
 //====================================
-
+void MediaLearnerLibTests::testExtractorPluginsExist(){
+    MediaLearner::MediaLearnerLib mediaLearner;
+    QMap<QString, MediaLearner::PluginSequenceExtractor*>
+            availableExtractor
+            = MediaLearner
+            ::PluginSequenceExtractor
+            ::getExtractors();
+    int nExtractors = availableExtractor.size();
+    QVERIFY(nExtractors > 0);
+}
+//====================================
+void MediaLearnerLibTests::testSequencesExtractions(){
+    MediaLearner::MediaLearnerLib mediaLearner;
+    MediaLearner::SequenceExtractor *extractor
+            = mediaLearner.getSequenceExtractor();
+    QMap<QString, MediaLearner::PluginSequenceExtractor*>
+            availableExtractors
+            = MediaLearner
+            ::PluginSequenceExtractor
+            ::getExtractors();
+    MediaLearner::PluginSequenceExtractor*
+            firstExtractor
+            = availableExtractors
+            .values()
+            .first();
+    extractor->setExtractor(firstExtractor);
+    QString videoFilePath
+            = "/home/cedric/Vidéos/musique_21sec.mp4";
+    //videoFilePath
+            //= "/home/cedric/Vidéos/toBeginAgain.mp3";
+    extractor->setMediaSource(
+                videoFilePath);
+    extractor->analyseMediaSource();
+    //*
+    //QThread::sleep(5);
+    QTime finalTime
+            = QTime::currentTime().addSecs(15);
+    while(QTime::currentTime() < finalTime){
+        QCoreApplication::processEvents(
+                    QEventLoop::AllEvents,
+                    100);
+    }
+    //*/
+    //extractor->waitForExtractionFinished();
+    QSharedPointer<QList<MediaLearner::Sequence> >
+            sequences
+            = extractor->getAllSequences();
+    int nSequences = sequences->size();
+    qDebug() << "nSequences: " << nSequences;
+    QVERIFY(nSequences > 0);
+}
+//====================================
 QTEST_MAIN(MediaLearnerLibTests)
 
 #include "tst_medialearnerlibtests.moc"
