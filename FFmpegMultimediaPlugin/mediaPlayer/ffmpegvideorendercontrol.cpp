36cc6232166621c127d46728df82389ac89ad34d
cedric <bcedrix@gmail.com>
2014-01-04 23:29:25 +0100

    #14 The fake media player plugin now works.

--- FFmpegMultimediaPlugin/mediaPlayer/ffmpegvideorendercontrol.cpp
+++ FFmpegMultimediaPlugin/mediaPlayer/ffmpegvideorendercontrol.cpp
@@ -1,6 +1,8 @@
 #include "ffmpegvideorendercontrol.h"
 
 #include <QAbstractVideoSurface>
+#include <QVideoSurfaceFormat>
+#include <QDebug>
 
 //====================================
 FFmpegVideoRenderControl::FFmpegVideoRenderControl(
@@ -22,6 +24,27 @@ void FFmpegVideoRenderControl::setDisplayedFrame(
         QVideoFrame frame){
     if(this->_surface != NULL){
         //TODO warning if format not supported
+        QVideoFrame::PixelFormat frameFormat
+                = frame.pixelFormat();
+        QVideoSurfaceFormat surfaceFormat
+                = this->_surface->surfaceFormat();
+        bool isSurfaceFormatValid = surfaceFormat.isValid();
+        qDebug() << "isSurfaceFormatValid: "<< isSurfaceFormatValid;
+        qDebug() << "frameFormat: "<< frameFormat;
+        qDebug() << "surfaceFormat: "<< surfaceFormat;
+        if(!isSurfaceFormatValid){
+            QSize frameSize = frame.size();
+            QVideoSurfaceFormat rightSurfaceFormat(
+                        frameSize,
+                        frameFormat);
+            bool formatSet = this->_surface->start(rightSurfaceFormat);
+            qDebug() << "formatSet: "<< formatSet;
+
+        }
+        QList<QVideoFrame::PixelFormat> supportedFormats
+                = this->_surface->supportedPixelFormats();
+        bool isFormatSupported = supportedFormats.contains(frameFormat);
+        qDebug() << "isFormatSupported: "<< isFormatSupported;
         this->_surface->present(
                     frame);
     }
